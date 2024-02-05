3d3b505a218791136073237ac4e3663cfe1ded21
MaxKov86 <max.kov86@ukr.net>
2024-02-05 22:20:41 +0200

    update files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,41 @@
 import "./App.css";
-import { favouriteBooks } from "./data";
+import favouriteBooks from "./data.json";
 import { BookList } from "./BookList/BookList";
 import { Alert } from "./Alert/Alert";
 import { UserMenu } from "./UserMenu/UserMenu";
+import { Button } from "./Button/Button";
+import { ClickCounter } from "./CounterClicks/CounterClicks";
+import { useState, useEffect } from "react";
 
 export const App = () => {
+  const [clicks, setClicks] = useState(0);
+  const [values, setValues] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [clickValue, setClickValue] = useState(0);
+
+  const updateX = () => {
+    setValues({
+      ...values,
+      x: values.x + 1,
+    });
+  };
+
+  const updateY = () => {
+    setValues({
+      ...values,
+      y: values.y + 1,
+    });
+  };
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+  useEffect(() => {
+    document.title = `You clicked ${clickValue} times`;
+  });
+
   return (
     <>
       <h1>Books of the week</h1>
@@ -22,6 +53,18 @@ export const App = () => {
         There was an error during your last transaction
       </Alert>
       <UserMenu name="Max" />
+      <Button />
+      <ClickCounter value={clicks} onUpdate={handleClick} />
+      <ClickCounter value={clicks} onUpdate={handleClick} />
+      <p>
+        x: {values.x}, y: {values.y}
+      </p>
+      <button onClick={updateX}>Update x</button>
+      <button onClick={updateY}>Update y</button>
+
+      <button onClick={() => setClickValue(clickValue + 1)}>
+        You clicked {clickValue} times
+      </button>
     </>
   );
 };
